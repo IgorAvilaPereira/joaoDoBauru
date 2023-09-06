ecf0f066971a5146ae7db850f4649d42856c3287
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-09-06 19:29:13 -0300

    ok

---
+++ demo/src/main/java/com/example/demo/controllers/FuncionarioController.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Funcionario;
+import com.example.demo.repositories.FuncionarioRepository;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ *
+ * @author iapereira
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("funcionarios")
+public class FuncionarioController {
+
+    private final FuncionarioRepository funcionarioRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Funcionario>> listar() {
+        return ResponseEntity.ok().body(funcionarioRepository.findAll());
+    }
+
+    // @PostMapping
+    // public ResponseEntity<Funcionario> inserir(@RequestBody Funcionario cli) {
+
+    //     funcionarioRepository.inserir(cli);
+
+    //     return ResponseEntity.ok().body(cli);
+    // }
+
+    // @PutMapping
+    // public ResponseEntity<Funcionario> atualizar(@RequestBody Funcionario cli) {
+
+    //     funcionarioRepository.atualizar(cli);
+
+    //     return ResponseEntity.ok().body(cli);
+    // }
+
+    // @DeleteMapping("/{id}")
+    // public ResponseEntity<?> deletar(@PathVariable int id) {
+    //     this.funcionarioRepository.deletar(id);
+    //     return ResponseEntity.ok().build();
+    // }
+
+}
---
+++ demo/src/main/java/com/example/demo/repositories/FuncionarioRepository.java
@@ -0,0 +1,141 @@
+package com.example.demo.repositories;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entities.Funcionario;
+import com.example.demo.repositories.mappers.FuncionarioRowMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class FuncionarioRepository {
+
+  private final JdbcTemplate jdbcTemplate;
+  private final FuncionarioRowMapper funcionarioRowMapper;
+
+  public List<Funcionario> findAll() {
+    String sql = """
+            SELECT
+            f.id as f_id,
+            f.nome as f_nome,
+            f.cpf as f_cpf,
+            f.endereco as f_endereco,
+            f.telefone as f_telefone           
+            FROM
+              funcionario f
+        """;
+    final List<Funcionario> Funcionarios = jdbcTemplate.query(sql, funcionarioRowMapper);
+    return Funcionarios;
+  }
+
+//   public Funcionario findById(int id) {
+//     String sql = """
+//         SELECT
+//           c.id as c_id,
+//           c.nome as c_nome,
+//           c.cpf as c_cpf,
+//           c.telefone as c_telefone,
+//           c.rua as c_rua,
+//           c.bairro as c_bairro,
+//           c.numero as c_numero,
+//           c.complemento as c_complemento,
+//           c.cep as c_cep
+//         FROM
+//           Funcionario c
+//         WHERE
+//           id = ?
+//           """;
+//     Funcionario Funcionario = jdbcTemplate.queryForObject(sql, funcionarioRowMapper, id);
+//     return Funcionario;
+//   }
+
+//   private final String DELETE_SQL = """
+//       BEGIN;
+//       DELETE FROM item WHERE pedido_id in (
+//         SELECT id FROM pedido WHERE Funcionario_id = ?
+//       );
+//       DELETE FROM pedido WHERE Funcionario_id = ?;
+//       DELETE FROM Funcionario WHERE id = ?; COMMIT;
+//       """;
+
+//   public void deletar(int id) {
+//     jdbcTemplate.update(DELETE_SQL, id, id, id);
+//   }
+
+//   public Funcionario inserir(Funcionario c) {
+//     String sql = """
+//         INSERT INTO Funcionario (nome, cpf,telefone,rua,bairro,numero,complemento,cep)
+//         VALUES (?, ?,?,?,?,?,?,?)
+//         """;
+
+//     KeyHolder keyHolder = new GeneratedKeyHolder();
+
+//     int insertsCount = jdbcTemplate.update(connection -> {
+//       PreparedStatement ps = connection
+//           .prepareStatement(sql, new String[] { "id" });
+//       ps.setString(1, c.getNome());
+//       ps.setString(2, c.getCpf());
+//       ps.setString(3, c.getTelefone());
+//       ps.setString(4, c.getEndereco().getRua());
+//       ps.setString(5, c.getEndereco().getBairro());
+//       ps.setString(6, c.getEndereco().getNumero());
+//       ps.setString(7, c.getEndereco().getComplemento());
+//       ps.setString(8, c.getEndereco().getCep());
+
+//       return ps;
+//     }, keyHolder);
+
+//     Number key = keyHolder.getKey();
+//     if (insertsCount == 1) {
+//       if (key != null) c.setId((Integer) key);
+//     }
+//     return c;
+//   }
+
+//   public void atualizar(Funcionario c) {
+
+//     jdbcTemplate.update("""
+//           UPDATE Funcionario
+//           SET  nome=?, cpf=?, telefone=?,
+//           rua=?, bairro=?, numero=?, complemento=?, cep=?
+//         WHERE id=?;
+
+//             """, c.getNome(), c.getCpf(), c.getTelefone(), c.getEndereco().getRua(),
+//         c.getEndereco().getBairro(), c.getEndereco().getNumero(),
+//         c.getEndereco().getComplemento(), c.getEndereco().getCep(),
+//         c.getId());
+
+//   }
+
+  /*
+   * private final String INSERT_SQL = """
+   * INSERT INTO Funcionario
+   * (nome, cpf, telefone, rua, bairro, complemento, cep)
+   * VALUES (?, ?, ?, ?, ?, ?, ?) RETURNING id;
+   * """;
+   */
+  /*
+   * // aqui ta dando erro..
+   * public void inserir(Funcionario c) {
+   * final Integer id = jdbcTemplate.query(INSERT_SQL,
+   * c.getNome(),
+   * c.getCpf(),
+   * c.getTelefone(),
+   * c.getEndereco().getRua(),
+   * c.getEndereco().getBairro(),
+   * c.getEndereco().getComplemento(),
+   * c.getEndereco().getCep(),
+   * (rs, rowNum) -> rs.getInt("id")
+   * );
+   * c.setId(id);
+   * }
+   */
+
+}
